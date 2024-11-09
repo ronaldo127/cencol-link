a4eea386d6d1a05d7bb433033c1bc319db4a6898
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2024-11-09 18:50:54 -0500

    Fix: deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "./dist", // Ensure output is in a `dist` folder in the root
+  },
+  base: "/cencol-link/", // Make sure this matches the repository name
+});
